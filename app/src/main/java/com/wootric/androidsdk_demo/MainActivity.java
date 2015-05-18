b854d92c6c5458949b9a918459684f187fb38e4a
Maciej Witowski <witowski.maciej@gmail.com>
2015-05-18 10:43:25 +0200

    Fix demo show survey button

--- app/src/main/java/com/wootric/androidsdk_demo/MainActivity.java
+++ app/src/main/java/com/wootric/androidsdk_demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.wootric.androidsdk_demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.wootric.androidsdk.Wootric;
 
@@ -10,6 +11,7 @@ public class MainActivity extends Activity {
     private static final String CLIENT_ID = "CLIENT ID";
     private static final String CLIENT_SECRET = "CLIENT SECRET";
     private static final String ACCOUNT_TOKEN = "ACCOUNT TOKEN";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,19 @@ public class MainActivity extends Activity {
                 .user(CLIENT_ID, CLIENT_SECRET, ACCOUNT_TOKEN)
                 .endUser("END USER EMAIL", "ORIGIN URL")
                 .survey();
+    }
+
+    public void showSurvey(View view) {
+        Wootric.with(this)
+                .user(CLIENT_ID, CLIENT_SECRET, ACCOUNT_TOKEN)
+                .endUser("END USER EMAIL", "ORIGIN URL")
+                .forceSurvey()
+                .survey();
+    }
+
+    @Override
+    protected void onStop() {
+        Wootric.stop();
+        super.onStop();
     }
 }
