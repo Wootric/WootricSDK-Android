0e8361c43233cb262db71d16831b2006c061fcc6
Maciej Witowski <witowski.maciej@gmail.com>
2015-09-09 10:58:01 +0200

    Keep score views in array

--- androidsdk/src/main/java/com/wootric/androidsdk/views/RatingLayout.java
+++ androidsdk/src/main/java/com/wootric/androidsdk/views/RatingLayout.java
@@ -22,6 +22,7 @@ public class RatingLayout extends RelativeLayout
     private Context mContext;
 
     private RatingBar mRatingBar;
+    private LinearLayout mScoreLayout;
 
     private int mScoreLayoutPaddingHorizontal;
     private float mScoreTextSizeSelected;
@@ -30,8 +31,11 @@ public class RatingLayout extends RelativeLayout
     private int mColorSelected;
     private int mColorNotSelected;
 
+    private int mScoresCount;
+
     private static final int SCORE_LAYOUT_ID = 1;
-    private static final int SCORE_TEXT_VIEW_ID_OFFSET = 100;
+
+    private TextView[] mScoreViews;
 
     public RatingLayout(Context context) {
         super(context);
@@ -51,6 +55,14 @@ public class RatingLayout extends RelativeLayout
     private void init(Context context) {
         mContext = context;
 
+        initResources();
+        initScoreLayout();
+        initRatingBar();
+
+        mRatingBar.setOnScoreChangedListener(this);
+    }
+
+    private void initResources() {
         Resources res = mContext.getResources();
         mColorNotSelected = res.getColor(R.color.wootric_dark_gray);
         mColorSelected = res.getColor(R.color.wootric_brand_color);
@@ -61,10 +73,27 @@ public class RatingLayout extends RelativeLayout
         mScoreTextSizeSelected = res.getDimension(R.dimen.wootric_selected_score_text_size);
         mScoreTextSizeNotSelected = res.getDimension(R.dimen.wootric_not_selected_score_text_size);
 
-        initScoreLayout();
-        initRatingBar();
+        mScoresCount = res.getInteger(R.integer.wootric_max_score) + 1;
+    }
 
-        mRatingBar.setOnScoreChangedListener(this);
+    private void initScoreLayout() {
+        mScoreLayout = new LinearLayout(mContext);
+        mScoreLayout.setId(SCORE_LAYOUT_ID);
+
+        mScoreLayout.setOrientation(LinearLayout.HORIZONTAL);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mScoreLayout.setLayoutParams(layoutParams);
+        mScoreLayout.setPadding(mScoreLayoutPaddingHorizontal, 0, mScoreLayoutPaddingHorizontal, 0);
+
+        mScoreViews = new TextView[mScoresCount];
+        for(int score = 0; score < mScoreViews.length; score++) {
+            TextView scoreView = buildScoreView(score);
+            mScoreViews[score] = scoreView;
+            mScoreLayout.addView(scoreView);
+        }
+
+        addView(mScoreLayout);
     }
 
     private void initRatingBar() {
@@ -73,35 +102,14 @@ public class RatingLayout extends RelativeLayout
         LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT);
 
-        layoutParams.addRule(BELOW, SCORE_LAYOUT_ID);
-
+        layoutParams.addRule(BELOW, mScoreLayout.getId());
         mRatingBar.setLayoutParams(layoutParams);
 
         addView(mRatingBar);
     }
 
-    private void initScoreLayout() {
-        LinearLayout scoreLayout = new LinearLayout(mContext);
-        scoreLayout.setId(SCORE_LAYOUT_ID);
-
-        scoreLayout.setOrientation(LinearLayout.HORIZONTAL);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        scoreLayout.setLayoutParams(layoutParams);
-        scoreLayout.setPadding(mScoreLayoutPaddingHorizontal, 0, mScoreLayoutPaddingHorizontal, 0);
-
-        int maxScore = mContext.getResources().getInteger(R.integer.wootric_max_score);
-        for(int score = 0; score <= maxScore; score++) {
-            TextView scoreView = buildScoreView(score);
-            scoreLayout.addView(scoreView);
-        }
-
-        addView(scoreLayout);
-    }
-
     private TextView buildScoreView(int score) {
         TextView scoreView = new TextView(mContext);
-        scoreView.setId(SCORE_TEXT_VIEW_ID_OFFSET + score);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f);
         scoreView.setLayoutParams(params);
 
@@ -115,13 +123,17 @@ public class RatingLayout extends RelativeLayout
 
     @Override
     public void onScoreChanged(int oldScore, int newScore) {
+        updateSelectedScore(oldScore, newScore);
+    }
+
+    private void updateSelectedScore(int oldScore, int newScore) {
         if(oldScore != Constants.INVALID_ID) {
-            TextView oldScoreView = (TextView) findViewById(SCORE_TEXT_VIEW_ID_OFFSET + oldScore);
+            TextView oldScoreView = mScoreViews[oldScore];
             oldScoreView.setTextColor(mColorNotSelected);
             oldScoreView.setTextSize(ScreenUtils.pxToDp(mScoreTextSizeNotSelected));
         }
 
-        TextView newScoreView = (TextView) findViewById(SCORE_TEXT_VIEW_ID_OFFSET + newScore);
+        TextView newScoreView = mScoreViews[newScore];
         newScoreView.setTextColor(mColorSelected);
         newScoreView.setTextSize(ScreenUtils.pxToDp(mScoreTextSizeSelected));
     }
